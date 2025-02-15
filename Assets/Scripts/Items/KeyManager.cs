521758d3732fce36bc72436c2241711d1ac9a6f5
David Alfred <dnalfred20@gmail.com>
2025-02-15 22:26:42 +0800

    Added KeyManaged Script
    Added KeyManager Script to implement collectible keys.
    Updated prefab to include new script.

--- Assets/Scripts/Items/KeyManager.cs
+++ Assets/Scripts/Items/KeyManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class KeyManager : MonoBehaviour
+public class KeyManager : MonoBehaviour, DataInterface
 {
-    // Start is called before the first frame update
-    void Start()
+private PlayerData playerData;
+    
+    private int keyPoints = 100; //points for each key collected
+    [SerializeField] private string id;
+    private bool isCollected = false;
+    public UnityEvent OnKeyCollected;
+
+    private void Awake()
     {
-        
+        //find player object's score controller
+        playerData = FindObjectOfType<PlayerData>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Generate guid for id")]
+    private void GenerateGuid()
     {
-        
+        id = System.Guid.NewGuid().ToString();
+    }
+
+    public void LoadData(GameData data)
+    {
+        data.itemsCollected.TryGetValue(id, out isCollected);
+        if(isCollected)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        if(data.itemsCollected.ContainsKey(id))
+        {
+            data.itemsCollected.Remove(id);
+        }
+        data.itemsCollected.Add(id, isCollected);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //On collision with the player, the item is removed and player's score is increased
+        if(collider.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            playerData.AddScore(keyPoints);
+            isCollected = true;
+            OnKeyCollected.Invoke();
+        }
     }
 }
