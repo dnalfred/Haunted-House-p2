75672e5592cb99f000e9993511cb1639391d31b1
David Alfred <dnalfred20@gmail.com>
2025-02-15 18:37:23 +0800

    Update HealthDisplay.cs
    Updated and simplified HealthDisplay script to better rely on events.

--- Assets/Scripts/UI/HealthDisplay.cs
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -8,12 +8,12 @@ public class HealthDisplay : MonoBehaviour
     public int displayHealth;
     public Image[] hearts;
 
-    private void Update() 
+    private void ShowHearts(int health)
     {
         //displays hearts from hearts array based on current displayHealth value
         for(int i = 0; i < hearts.Length; i++) 
         {
-            if(i < displayHealth) 
+            if(i < health) 
             {
                 hearts[i].enabled = true;
             } 
@@ -27,6 +27,6 @@ public class HealthDisplay : MonoBehaviour
     //update displayHealth value
     public void updateHealth(PlayerData playerData)
     {
-        displayHealth = playerData.health;
+        ShowHearts(playerData.health);
     }
 }
