f3287b32543b65c14fa072f0813eaabca6e41ce4
David Alfred <dnalfred20@gmail.com>
2025-02-20 19:21:45 +0800

    Updated Main Menu
    Updated Main Menu layout and colours.
    Added MainMenuDisplay script with functions for Main Menu buttons (Play, High Scores and Quit).
    Reorganised UI scripts into folders.

---
+++ Assets/Scripts/UI/MainMenu/MainMeneDisplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMeneDisplay : MonoBehaviour
+{
+    //For Menu Play button
+    public void PlayButton()
+    {
+        SceneManager.LoadScene ("LevelScene", LoadSceneMode.Single);
+    }
+
+    //For Menu High Scores button
+    public void ScoresButton()
+    {
+        Debug.Log("Scores button clicked!"); //delete
+    }
+
+    //For Menu Quit button
+    public void QuitButton()
+    {
+        Debug.Log("Quit button clicked!"); //delete
+        Application.Quit();
+    }
+}
