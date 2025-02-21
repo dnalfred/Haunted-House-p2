1c2358742a924fb79c0a26543822184486898fc9
David Alfred <dnalfred20@gmail.com>
2025-02-21 11:55:59 +0800

    Fixed Key DIsplay Bug
    Updated KeyDisplay script to correctly display key only when collected.

--- Assets/Scripts/UI/GameLevels/KeyDisplay.cs
+++ Assets/Scripts/UI/GameLevels/KeyDisplay.cs
@@ -7,16 +7,24 @@ public class KeyDisplay : MonoBehaviour
     private void Start()
     {
         //Hide key when game starts
-        HideKey();
+        // HideKey();
     }
 
     public void HideKey()
     {
+        Debug.Log("HideKey triggered");
         gameObject.SetActive(false);
     }
 
     public void ShowKey(PlayerData playerData)
     {
-        gameObject.SetActive(true);
+        if(playerData.isKeyCollected == 1)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
