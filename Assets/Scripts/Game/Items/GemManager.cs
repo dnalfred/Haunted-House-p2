0f6e9c334b673b50ad54c22fa85ecc57a7fba3b6
David Alfred <dnalfred20@gmail.com>
2025-03-06 09:37:29 +0800

    Added Gem Collection Animation
    Updated GemManager script to trigger an animation when a gem is collected.
    Updated Gem prefab with a RigidBody2D to control movement.
    Minor update to level scene to make movement easire near the top of the ladder.

--- Assets/Scripts/Game/Items/GemManager.cs
+++ Assets/Scripts/Game/Items/GemManager.cs
@@ -5,17 +5,43 @@ using UnityEngine.Events;
 
 public class GemManager : MonoBehaviour, DataInterface
 {
+    private Rigidbody2D body;
     private PlayerData playerData;
     
     private int itemPoints = 100; //points for each item collected
     [SerializeField] private string id;
     private bool isCollected = false;
     public UnityEvent OnGemCollected;
+    private float startingPosY;
 
     private void Awake()
     {
+        //Find component on the item object
+        body = gameObject.GetComponent<Rigidbody2D>();
+
         //find player object's score controller
         playerData = FindObjectOfType<PlayerData>();
+    }
+
+    private void Start()
+    {
+        //Set item gravity scale to 0 at start
+        body.gravityScale = 0;
+
+        //Set Y starting position
+        startingPosY = body.position.y;
+    }
+
+    private void Update()
+    {
+        //Destroy item object if isCollected and has moved a certain distance
+        if(isCollected)
+        {
+            if((body.position.y - startingPosY)>1)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     [ContextMenu("Generate guid for id")]
@@ -47,10 +73,18 @@ public class GemManager : MonoBehaviour, DataInterface
         //on collision with the player, the item is destroyed and player's score is increased
         if(collider.gameObject.tag == "Player")
         {
+            //Play sound on collection
             SoundManager.instance.PlaySoundFXClip(SoundManager.instance.gemSound, transform);
-            Destroy(gameObject);
+
+            //Cause item to "jump" up on collection
+            body.gravityScale = 1;
+            body.velocity = new Vector2(0, 6);
+
+            //Add score and set isCollected
             playerData.AddScore(itemPoints);
             isCollected = true;
+
+            //Invoke OnGemCollected event
             OnGemCollected.Invoke();
         }
     }
