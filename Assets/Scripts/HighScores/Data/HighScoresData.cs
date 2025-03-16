8d8e55773b2554783d705b2450edc9a7bbaca248
David Alfred <dnalfred20@gmail.com>
2025-03-16 19:42:25 +0800

    Update HighScoresData.cs
    Updated HighScoresData script to implement list of sorted high scores (instead of random data).

--- Assets/Scripts/HighScores/Data/HighScoresData.cs
+++ Assets/Scripts/HighScores/Data/HighScoresData.cs
@@ -7,6 +7,8 @@ public class HighScoresData : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
     float templateHeight = 60f;
 
     private void Awake()
@@ -21,51 +23,87 @@ public class HighScoresData : MonoBehaviour
 
     private void Start()
     {
-        DisplayScores();
+        CreateNewLists();
     }
 
-    private void DisplayScores()
+    private void CreateNewLists()
     {
-        for(int i=0; i<10; i++)
+        //Create new highscoreEntryList (for testing)
+        highscoreEntryList = new List<HighscoreEntry>()
         {
-            //Instatiat a new highscore entry at the correct location
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight*i);
-            entryTransform.gameObject.SetActive(true);
+            new HighscoreEntry { score = 1233, name = "AAA"},
+            new HighscoreEntry { score = 455, name = "BBB"},
+            new HighscoreEntry { score = 102, name = "CCC"},
+            new HighscoreEntry { score = 783, name = "DDD"},
+            new HighscoreEntry { score = 1045, name = "EEE"},
+            new HighscoreEntry { score = 287, name = "FFF"},
+            new HighscoreEntry { score = 65, name = "GGG"},
+            new HighscoreEntry { score = 953, name = "HHH"},
+            new HighscoreEntry { score = 627, name = "III"},
+            new HighscoreEntry { score = 1312, name = "JJJ"},
+        };
 
-            //Set rank string
-            int rank = i+1;
-            string rankString;
-            switch (rank)
+        //Sort List
+        for (int i=0; i<highscoreEntryList.Count; i++)
+        {
+            for (int j=i+1; j<highscoreEntryList.Count; j++)
             {
-                default: rankString = rank + "th"; break;
-                case 1: rankString = "1st"; break;
-                case 2: rankString = "2nd"; break;
-                case 3: rankString = "3rd"; break;
-            }
-            entryTransform.Find("posText").GetComponent<TMP_Text>().text = rankString;
-            if(rank<4)
-            {
-                entryTransform.Find("posText").GetComponent<TMP_Text>().color = new Color32 (255, 215, 95, 255);
-            }
-
-            //Set name string
-            string nameString = "AAA";
-            entryTransform.Find("nameText").GetComponent<TMP_Text>().text = nameString;
-            if(rank<4)
-            {
-                entryTransform.Find("nameText").GetComponent<TMP_Text>().color = new Color32 (255, 215, 95, 255);
-            }
-
-            //Set score string
-            int score = Random.Range (0, 1000);
-            string scoreString = score.ToString();
-            entryTransform.Find("scoreText").GetComponent<TMP_Text>().text = scoreString;
-            if(rank<4)
-            {
-                entryTransform.Find("scoreText").GetComponent<TMP_Text>().color = new Color32 (255, 215, 95, 255);
+                //Check if next element is larger
+                if(highscoreEntryList[j].score > highscoreEntryList[i].score)
+                {
+                    //Swap
+                    HighscoreEntry temporaryEntry = highscoreEntryList[i];
+                    highscoreEntryList[i] = highscoreEntryList[j];
+                    highscoreEntryList[j] = temporaryEntry;
+                }
             }
         }
+
+        //Create new highscoreEntryTransformList
+        highscoreEntryTransformList = new List<Transform>();
+
+        //Populate high score table
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighScoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+    }
+
+    //Create a single highscore entry
+    private void CreateHighScoreEntryTransform (HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight*transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        //Set rank
+        int rank = transformList.Count+1;
+        string rankString;
+        switch (rank)
+        {
+            default: rankString = rank + "th"; break;
+            case 1: rankString = "1st"; break;
+            case 2: rankString = "2nd"; break;
+            case 3: rankString = "3rd"; break;
+        }
+        entryTransform.Find("posText").GetComponent<TMP_Text>().text = rankString;
+
+        //Set score
+        int score = highscoreEntry.score;
+        string scoreString = score.ToString();
+        entryTransform.Find("scoreText").GetComponent<TMP_Text>().text = scoreString;
+
+        //Set name
+        string nameString = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<TMP_Text>().text = nameString;
+
+        transformList.Add(entryTransform);
+    }
+
+    private class HighscoreEntry
+    {
+        public string name;
+        public int score;
     }
 }
